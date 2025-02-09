650bde56276633a07791c61209752bf4daf8aa5c
Kay <info@beckmann-md.de>
2025-02-09 15:25:03 +0100

    create authView

--- frontend/src/views/AuthView.vue
+++ frontend/src/views/AuthView.vue
@@ -1,3 +1,12 @@
 <template>
   <h1>AUTH</h1>
+  <LoginComponent />
+  <RegisterComponent />
 </template>
+
+<script setup>
+import LoginComponent from '@/components/Auth/LoginComponent.vue';
+import RegisterComponent from '@/components/Auth/RegisterComponent.vue';
+</script>
+
+<style scoped></style>
