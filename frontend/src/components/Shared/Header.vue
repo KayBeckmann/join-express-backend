f325509b3081da237ee41b3a81ad91fc852df9d7
Kay <info@beckmann-md.de>
2025-02-24 20:27:36 +0100

    create header

---
+++ frontend/src/components/Shared/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <header id="header">
+    <h1>Task Manager</h1>
+  </header>
+</template>
+
+<script setup></script>
+
+<style lang="css" scoped>
+header {
+  background-color: lightgray;
+}
+</style>
